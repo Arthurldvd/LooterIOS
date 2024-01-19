6fc1182fdbebc846d69b432f51166bbdbe40b4a0
Arthur <arthur.le.devedec@gmail.com>
2024-01-19 10:30:25 +0100

    Initial Commit

---
+++ LooterIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LooterIOS
+//
+//  Created by Arthur LE-DEVEDEC on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LooterIOS/LooterIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooterIOSApp.swift
+//  LooterIOS
+//
+//  Created by Arthur LE-DEVEDEC on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LooterIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
